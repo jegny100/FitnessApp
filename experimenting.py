906eae1adb522821156db233ac669fb1008a869d
jegny100 <jegny100@hhu.de>
2021-07-27 14:16:11 +0200

    added NavigationDrawer

--- experimenting.py
+++ experimenting.py
@@ -1,25 +1,75 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
 
 KV = '''
-MDBoxLayout:
-    orientation: "vertical"
+<ContentNavigationDrawer>:
+
+    ScrollView:
+
+        MDList:
+
+            OneLineListItem:
+                text: "Screen 1"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 1"
+
+            OneLineListItem:
+                text: "Screen 2"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 2"
+
+
+MDScreen:
 
     MDToolbar:
-        title: "MDToolbar"
-        left_action_items: [["menu", lambda x: app.callback()]]
-        right_action_items: [["dots-vertical", lambda x: app.callback()]]
+        id: toolbar
+        pos_hint: {"top": 1}
+        elevation: 10
+        title: "MDNavigationDrawer"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
 
-    MDLabel:
-        text: "Content"
-        halign: "center"
+    MDNavigationLayout:
+        x: toolbar.height
+
+        ScreenManager:
+            id: screen_manager
+
+            MDScreen:
+                name: "scr 1"
+
+                MDLabel:
+                    text: "Screen 1"
+                    halign: "center"
+
+            MDScreen:
+                name: "scr 2"
+
+                MDLabel:
+                    text: "Screen 2"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
 '''
 
 
-class Test(MDApp):
+class ContentNavigationDrawer(MDBoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class TestNavigationDrawer(MDApp):
     def build(self):
         return Builder.load_string(KV)
 
 
-Test().run()
+TestNavigationDrawer().run()
--- main.py
+++ main.py
@@ -1,7 +1,64 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.button import MDFillRoundFlatButton, MDIconButton
 from kivymd.uix.screen import Screen
+
+KV = '''
+<ContentNavigationDrawer>:
+    ScrollView:
+        MDList:
+            OneLineListItem:
+                text: "Aktivitäten"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "activities"
+                    
+            OneLineListItem:
+                text: "Statistiken"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "statistics"
+                    
+MDScreen:
+    MDToolbar:
+        id: toolbar
+        pos_hint: {"top":1}
+        elevation: 5
+        title: "FitnessApp"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+        
+    MDNavigationLayout:
+        x: toolbar.height
+        
+        ScreenManager:
+            id: screen_manager
+            
+            MDScreen:
+                name: "activities"
+                MDLabel:
+                    text: "Aktivitäten"
+                    halign: "center"
+            
+            MDScreen:
+                name: "statistics"
+                MDLabel:
+                    text: "Statistiken"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+'''
+
+class ContentNavigationDrawer(MDBoxLayout):
+    screen_manager = ObjectProperty()  # TODO check documentary
+    nav_drawer = ObjectProperty()
 
 
 class FitnessApp(MDApp):
@@ -24,7 +81,7 @@ class FitnessApp(MDApp):
                                 md_bg_color=(154 / 255.0, 212 / 255.0, 194 / 255.0, 1))
         homescreen.add_widget(plus_btn)
 
-        return homescreen
+        return Builder.load_string(KV)
 
 
 FitnessApp().run()
