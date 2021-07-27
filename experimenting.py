5139b58d804236f1d2df7f33addbc78b429529c8
jegny100 <jegny100@hhu.de>
2021-07-27 15:48:16 +0200

    experimenting with NavigationDrawer

--- experimenting.py
+++ experimenting.py
@@ -1,75 +1,129 @@
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty, ObjectProperty
 
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
 
 KV = '''
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
+
+
 <ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+
+    MDLabel:
+        text: "KivyMD library"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "kivydevelopment@gmail.com"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
 
     ScrollView:
 
-        MDList:
+        DrawerList:
+            id: md_list
 
-            OneLineListItem:
-                text: "Screen 1"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 1"
-
-            OneLineListItem:
-                text: "Screen 2"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 2"
 
 
 MDScreen:
 
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "MDNavigationDrawer"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-
     MDNavigationLayout:
-        x: toolbar.height
 
         ScreenManager:
             id: screen_manager
 
             MDScreen:
-                name: "scr 1"
 
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
+                BoxLayout:
+                    orientation: 'vertical'
 
-            MDScreen:
-                name: "scr 2"
+                    MDToolbar:
+                        title: "Navigation Drawer"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.
+        toggle()]]
 
-                MDLabel:
-                    text: "Screen 2"
-                    halign: "center"
-
+                    Widget:
+                
         MDNavigationDrawer:
             id: nav_drawer
+        
 
             ContentNavigationDrawer:
+                id: content_drawer
                 screen_manager: screen_manager
                 nav_drawer: nav_drawer
 '''
 
 
-class ContentNavigationDrawer(MDBoxLayout):
-    screen_manager = ObjectProperty()
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty()  # TODO check documentary
     nav_drawer = ObjectProperty()
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
 
 
 class TestNavigationDrawer(MDApp):
     def build(self):
         return Builder.load_string(KV)
+
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
 
 
 TestNavigationDrawer().run()
--- main.py
+++ main.py
@@ -64,22 +64,22 @@ class ContentNavigationDrawer(MDBoxLayout):
 class FitnessApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Teal"
-        homescreen = Screen()
+        activities = Screen()
 
         label = MDLabel(text='Hello', halign='center', valign='top',
                         theme_text_color='Custom',
                         text_color=(154 / 255.0, 212 / 255.0, 194 / 255.0, 1),
                         font_style='H1')
-        homescreen.add_widget(label)
+        activities.add_widget(label)
 
         btn_flat = MDFillRoundFlatButton(text='Aktivität hinzufügen',
                                          pos_hint={'center_x': 0.5, 'center_y': 0.25})
-        homescreen.add_widget(btn_flat)
+        activities.add_widget(btn_flat)
 
         plus_btn = MDIconButton(icon='plus',
                                 pos_hint={'center_x': 0.5, 'center_y': 0.125},
                                 md_bg_color=(154 / 255.0, 212 / 255.0, 194 / 255.0, 1))
-        homescreen.add_widget(plus_btn)
+        activities.add_widget(plus_btn)
 
         return Builder.load_string(KV)
 
