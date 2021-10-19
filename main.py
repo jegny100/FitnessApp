e66f4f6b0fd4c2c86dde781a6a605497c06dc61d
jegny100 <jegny100@hhu.de>
2021-10-19 15:02:14 +0200

    outsourced KV file & added dialog window

--- main.py
+++ main.py
@@ -2,77 +2,18 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.label import MDLabel, MDIcon
-from kivymd.uix.button import MDFillRoundFlatButton, MDIconButton
+from kivymd.uix.button import MDFillRoundFlatButton, MDIconButton, MDFlatButton
 from kivymd.uix.screen import Screen
+import main_kivy
 
 from kivy.core.window import Window
 Window.size = (350, 600)
 
-KV = '''
-<ContentNavigationDrawer>:
-    ScrollView:
-        MDList:
-            OneLineListItem:
-                text: "Aktivitäten"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "activities"
-                    
-            OneLineListItem:
-                text: "Statistiken"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "statistics"
-                    
-MDScreen:
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top":1}
-        elevation: 5
-        title: "FitnessApp"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-        
-    MDNavigationLayout:
-        x: toolbar.height
-        
-        ScreenManager:
-            id: screen_manager
-            
-            MDScreen:
-                name: "activities"
-                
-                MDBoxLayout:
-                    orientation: "vertical"
-                        
-                    MDLabel:
-                        text :'Hello'
-                        halign : 'center'
-                        valign : 'top'
-                        font_style : 'H3'  
-                        
-                    MDIconButton:
-                        icon: 'plus'
-                        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-                        md_bg_color : (154 / 255.0, 212 / 255.0, 194 / 255.0, 1)
-                        
-            
-            MDScreen:
-                name: "statistics"
-                MDLabel:
-                    text: "Statistiken"
-                    halign: "center"
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            ContentNavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-'''
 
 class ContentNavigationDrawer(MDBoxLayout):
-    screen_manager = ObjectProperty()  # TODO check documentary
+    screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
 
 
@@ -80,7 +21,18 @@ class FitnessApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Teal"
 
-        return Builder.load_string(KV)
+        return Builder.load_string(main_kivy.KV)
+
+    def show_dialog(self, obj):
+        self.dialog = MDDialog(title='Aktivität hinzufügen',
+                               text='Test test tesst', size_hint=(0.8, 1),
+                               buttons=[MDFlatButton(text='Close', on_release=self.close_dialog),
+                                        MDFlatButton(text='Save')]
+                               )
+        self.dialog.open()
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
 
 
 FitnessApp().run()
---
+++ main_kivy.py
@@ -0,0 +1,62 @@
+KV = '''
+<ContentNavigationDrawer>:
+    ScrollView:
+        MDList:
+            OneLineListItem:
+                text: "activities"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "activities"
+                    
+            OneLineListItem:
+                text: "statistics"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "statistics"
+                    
+MDScreen:
+    MDToolbar:
+        id: toolbar
+        pos_hint: {"top":1}
+        elevation: 5
+        title: "FitnessApp"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+        
+    MDNavigationLayout:
+        x: toolbar.height
+        
+        ScreenManager:
+            id: screen_manager
+            
+            MDScreen:
+                name: "activities"
+                
+                MDBoxLayout:
+                    orientation: "vertical"
+                        
+                    MDLabel:
+                        text :'Hello'
+                        halign : 'center'
+                        valign : 'top'
+                        font_style : 'H3'  
+                        
+                    MDIconButton:
+                        icon: 'plus'
+                        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+                        md_bg_color : (154 / 255.0, 212 / 255.0, 194 / 255.0, 1)
+                        on_press: app.show_dialog(self)
+                        
+            
+            MDScreen:
+                name: "statistics"
+                MDLabel:
+                    text: "Statistics"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+'''
