f8e7a3dbc25e78cd65eb9cf4b1f40638cc5be9ef
jegny100 <jegny100@hhu.de>
2021-10-19 15:48:01 +0200

    Update experimenting.py
    test

--- experimenting.py
+++ experimenting.py
@@ -8,12 +8,14 @@ Screen
         id: drop_item
         pos_hint: {'center_x': 0.5, 'center_y': 0.5}
         text: 'select items'
+        items: app.items
         on_release: self.set_item("vipin coding")    
 '''
 
 class Test(MDApp):
 
     def build(self):
+        self.items = ["a","b"]
         return Builder.load_string(KV)
 
 
