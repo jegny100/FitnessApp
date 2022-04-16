6bfdbe8df82d78dfb2937993d37441d90a82a5c9
jegny100 <jegny100@hhu.de>
2022-04-16 10:24:58 +0200

    added chat logic

--- trial_and_error.py
+++ trial_and_error.py
@@ -7,15 +7,33 @@ from datetime import datetime, timedelta
 logger_df = helper_functions.get_logger()
 activities_df = helper_functions.get_activity_collection()
 all_buddys_df = helper_functions.get_buddys()
-#print(all_buddys_df)
+# print(all_buddys_df)
 convo_list = []
 
 convo_buddy, activity = "Red Panda", "Liegestütze"
-#convo_buddy, activity = "Red Panda", "test"
+# convo_buddy, activity = "Red Panda", "test"
 
 csv_name = convo_buddy + "_workout_chat.csv"
 buddy_convo_df = pd.read_csv(csv_name)
 tag = "Intro"
+
+
+def get_dict_chat_variables():
+    logger_df = helper_functions.get_logger()
+    activities_df = helper_functions.get_activity_collection()
+    all_buddys_df = helper_functions.get_buddys()
+
+
+def get_string_variable(text_string):
+    # strip string
+    # ersetzte [] durch entsprechende variable
+    ## if [] ==[] -> [] = variablestring
+    ## if [] ==[] -> [] = variablestring
+    ## if [] ==[] -> [] = variablestring
+    # ...
+    # baues string wieder zusammen
+    return text_string
+
 
 while tag != 'nan':
     # filter by tag
@@ -35,26 +53,38 @@ while tag != 'nan':
 
     # filter by logged last week
     subset_logger_df = logger_df.loc[logger_df["activity"] == activity]
-    subset_logger_df = subset_logger_df.loc[pd.to_datetime(subset_logger_df["date"]) > (datetime.today()-timedelta(days=7))]
+    subset_logger_df = subset_logger_df.loc[
+        pd.to_datetime(subset_logger_df["date"]) > (datetime.today() - timedelta(days=7))]
     if subset_logger_df.shape[0] > 0:
         subset_buddy_convo_df = subset_buddy_convo_df.loc[subset_buddy_convo_df["logged last week"] != 0]
     else:
         subset_buddy_convo_df = subset_buddy_convo_df.loc[subset_buddy_convo_df["logged last week"] != 1]
 
     # filter by change
-    logger_df.loc[logger_df["activity"] == activity]
+    subset_logger_df = logger_df.loc[logger_df["activity"] == activity]
+    # instances of activities for the week before today and the week 2 weeks before today
+    one_week_subset_logger_df = subset_logger_df.loc[
+        pd.to_datetime(subset_logger_df["date"]) > (datetime.today() - timedelta(days=7))]
+    two_weeks_subset_logger_df = subset_logger_df.loc[
+        pd.to_datetime(subset_logger_df["date"]) <= (datetime.today() - timedelta(days=7))]
+    two_weeks_subset_logger_df = subset_logger_df.loc[
+        pd.to_datetime(subset_logger_df["date"]) > (datetime.today() - timedelta(days=14))]
 
+    if one_week_subset_logger_df.shape[0] > two_weeks_subset_logger_df.shape[0]:
+        subset_buddy_convo_df = subset_buddy_convo_df.loc[(subset_buddy_convo_df["change"] == 1) | (subset_buddy_convo_df["change"].isnull())]
+    if one_week_subset_logger_df.shape[0] == two_weeks_subset_logger_df.shape[0]:
+        subset_buddy_convo_df = subset_buddy_convo_df.loc[(subset_buddy_convo_df["change"] == 2) | (subset_buddy_convo_df["change"].isnull())]
 
-
-    # buddy_convo_df.loc[buddy_convo_df["logged last week"] == 1]
-    # buddy_convo_df.loc[buddy_convo_df["logged last week"] == 0]
+    if one_week_subset_logger_df.shape[0] < two_weeks_subset_logger_df.shape[0]:
+        subset_buddy_convo_df = subset_buddy_convo_df.loc[(subset_buddy_convo_df["change"] == 3) | (subset_buddy_convo_df["change"].isnull())]
 
     # randomly select a suitable successor from the remaining lines
     next_line = subset_buddy_convo_df.loc[random.choice(subset_buddy_convo_df.index)]
     tag = str(next_line["next tag"])
-
-    convo_list.append(next_line["Text"])
+    # variablen in string einfügen
+    new_chat_line = get_string_variable(next_line["Text"])
+    convo_list.append(new_chat_line)
     print(next_line["Text"])
 
 # print(convo_list)
-#convo_list.append()
+# convo_list.append()
