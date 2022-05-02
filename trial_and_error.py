520e78d2e9c0a70eb8c2aab3230f1607027aa113
jegny100 <jegny100@hhu.de>
2022-05-02 12:32:23 +0200

    Clean Up: delete unnecessary files

--- trial_and_error.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import helper_functions
-
-logger_df = helper_functions.get_logger()
-values = logger_df['activity'].value_counts()
-
-print(values)
-print("\n Activity  ",values.idxmin())
